1bda30b237cd3f9b02955bf1b18498bbb0f43009
devanshkhatri12 <dkkhatri941@gmail.com>
2026-02-06 17:14:30 +0530

    Lecture 2

---
+++ src/Lecture2.ts
@@ -0,0 +1,11 @@
+let drink = "chai"
+
+// type infrencing
+let age = 32
+
+// type annotation
+let bio:string = "aboutMe"
+bio = "mySelf"
+
+// cause error : assignable (not resolved by any ai tools)
+// bio = 1
